b068c15eb7a0b892b95ea8873e8b6a50fe479141
Dawson Ley <anarchonist@protonmail.com>
2018-10-29 23:22:39 +0000

    successfully piped image to its own file

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -25,3 +26,17 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
   //console.log("Result:", result);
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+.on('error', function (err) {
+  throw err;
+})
+.on('response', function(response) {
+  console.log('Response Status Code: ', response.statusCode, 'Response Message : ' + response.statusMessage + '\nResponse content: ' + response.headers['content-type']);
+})
+.pipe(fs.createWriteStream(filePath));
+
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "downloadedstuff.jpg");
